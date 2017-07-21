2ee560ccbb1794a69ff3dfc9197e4b7f63c2e724
MC <2590801556@qq.com>
2017-07-21 08:02:55 +0800

    是真的丑

---
+++ js/xiaomi.js
@@ -0,0 +1,8 @@
+var myswiper=new Swiper('.swiper-container',{
+			autoplay : 2000,
+			autoplayDisableOnInteraction : false,
+			loop : true,
+			pagination : '.swiper-pagination',
+			prevButton:'.swiper-button-prev',
+			nextButton:'.swiper-button-next'
+});
